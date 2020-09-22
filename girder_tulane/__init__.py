09fc62944a75cd00e56c0bb5151b4ea2e97174ec
Roni Choudhury <roni.choudhury@kitware.com>
2020-09-22 17:50:17 -0400

    Add HistomicsUI dependency

--- girder_tulane/__init__.py
+++ girder_tulane/__init__.py
@@ -6,5 +6,4 @@ class GirderPlugin(plugin.GirderPlugin):
     CLIENT_SOURCE_PATH = 'web_client'
 
     def load(self, info):
-        # add plugin loading logic here
-        pass
+        plugin.getPlugin('histomicsui').load(info)

28bb0727cd2f54aa7f82d9f19f6257e8c6b50377
Roni Choudhury <roni.choudhury@kitware.com>
2020-11-23 12:37:11 -0500

    Add experiment control buttons and behavior

--- annotation_tracker/web_client/panels/Experiments/index.js
+++ annotation_tracker/web_client/panels/Experiments/index.js
@@ -3,16 +3,36 @@ import Panel from '@girder/slicer_cli_web/views/Panel';
 import experiments from './experiments.pug';
 
 const Experiments = Panel.extend({
+  events: _.extend(Panel.prototype.events, {
+    'click .h-toggle-experiment': 'toggleExperiment',
+    'click .h-advance-task': 'advanceTask',
+  }),
+
   initialize() {
     console.log('Experiments.initialize()');
+
+    this.running = false;
+    this.task = 0;
   },
 
   render() {
     this.$el.html(experiments({
       id: 'experiments-panel',
+      running: this.running,
+      task: this.task,
     }));
 
     return this;
+  },
+
+  toggleExperiment(evt) {
+    this.running = !this.running;
+    this.render();
+  },
+
+  advanceTask(evt) {
+    this.task += 1;
+    this.render();
   },
 });
 

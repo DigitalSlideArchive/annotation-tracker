8a3bca179df6678c3e4396a2a2f80c6630d27c82
BryonLewis <Bryon.Lewis@kitware.com>
2021-03-11 18:21:34 -0500

    setup and ran through linter

--- annotation_tracker/web_client/panels/Experiments/index.js
+++ annotation_tracker/web_client/panels/Experiments/index.js
@@ -18,21 +18,21 @@ const Experiments = Panel.extend({
     }),
 
     initialize() {
-        this.running = false; //bool - current task running or stopped
-        this.experimentIndex  = 0; // int - index of current experiment
+        this.running = false; // bool - current task running or stopped
+        this.experimentIndex = 0; // int - index of current experiment
         this.experiment = null; //
-        this.taskIndex = -1; //current indesk into task
+        this.taskIndex = -1; // current indesk into task
         this.task = null; // task
-        this.experiments = null; //List of experiments if we have more than one
+        this.experiments = null; // List of experiments if we have more than one
         this.sectionExpanded = {
             'task': false,
             'description': false,
             'input': false
-        }; //Utilized for toggling the different sections in the experiments panel
+        }; // Utilized for toggling the different sections in the experiments panel
     },
 
     setFolderId(folderId) {
-        //fetch the metadata from the FolderID
+        // fetch the metadata from the FolderID
         const folderModel = new FolderModel();
         folderModel.set({
             _id: folderId
@@ -47,9 +47,9 @@ const Experiments = Panel.extend({
     processMetadata(metadata) {
         if (metadata.experiments !== undefined) {
             // TODO: JSON Schema validation at some point to ensure we have all necessary data
-            this.experimentIndex  = 0;
-            this.taskIndex = 0;    
-            this.experiments =  metadata.experiments;
+            this.experimentIndex = 0;
+            this.taskIndex = 0;
+            this.experiments = metadata.experiments;
             this.experiment = this.experiments[this.experimentIndex];
             this.task = this.experiment.tasks[this.taskIndex];
             this.render();
@@ -57,7 +57,6 @@ const Experiments = Panel.extend({
     },
 
     render() {
-
         // Lets not render until we have metadata or we have a future loading state?
         if (this.experiments) {
             this.$el.html(experiments({
@@ -75,10 +74,10 @@ const Experiments = Panel.extend({
     setCurrentTask(evt) {
         if (this.running) {
             this.running = !this.running;
-            activityLogger.log('task', {running: this.running, task: this.task, experiment: this.experiment.name, 'taskAction': 'toggle'});
+            activityLogger.log('task', { running: this.running, task: this.task, experiment: this.experiment.name, 'taskAction': 'toggle' });
         }
-        const index = $(evt.currentTarget).data('task-index')
-        if (index < this.experiment.tasks.length){
+        const index = this.$(evt.currentTarget).data('task-index');
+        if (index < this.experiment.tasks.length) {
             this.task = this.experiment.tasks[index];
             this.taskIndex = index;
             this.render();
@@ -86,37 +85,37 @@ const Experiments = Panel.extend({
     },
     toggleTask(evt) {
         this.running = !this.running;
-        activityLogger.log('task', {running: this.running, task: this.task, experiment: this.experiment.name, 'taskAction': 'toggle'});
+        activityLogger.log('task', { running: this.running, task: this.task, experiment: this.experiment.name, 'taskAction': 'toggle' });
         this.render();
     },
     nextTask() {
-        if (this.taskIndex < this.experiment.tasks.length - 1 ) {
+        if (this.taskIndex < this.experiment.tasks.length - 1) {
             if (this.running) {
                 this.running = !this.running;
-                activityLogger.log('task', {running: this.running, task: this.task, experiment: this.experiment.name, 'taskAction': 'toggle'});
+                activityLogger.log('task', { running: this.running, task: this.task, experiment: this.experiment.name, 'taskAction': 'toggle' });
             }
-            this.taskIndex +=1 ;
+            this.taskIndex += 1;
             this.task = this.experiment.tasks[this.taskIndex];
             this.render();
         } else {
             this.stopExperiment();
-            this.task = null;            
+            this.task = null;
             this.render();
         }
     },
     stopExperiment(evt) {
-        if (this.taskIndex !== -1){
+        if (this.taskIndex !== -1) {
             this.running = false;
-            activityLogger.log('task', {running: this.running, task: this.task, experiment: this.experiment.name, 'taskAction': 'toggle'});
+            activityLogger.log('task', { running: this.running, task: this.task, experiment: this.experiment.name, 'taskAction': 'toggle' });
             this.taskIndex = -1;
             this.task = null;
-            this.render();    
+            this.render();
         }
     },
     _toggleSectionList(evt) {
-        const target = $(evt.currentTarget).data('target');
+        const target = this.$(evt.currentTarget).data('target');
         if (this.sectionExpanded[target] !== undefined) {
-            this.sectionExpanded[target] = !this.sectionExpanded[target]
+            this.sectionExpanded[target] = !this.sectionExpanded[target];
         }
         this.render();
     }
